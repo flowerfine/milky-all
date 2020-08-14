51265cbdcb0e9c91e07533a6b00843b5346f2c5c
wangqi <1942460489@qq.com>
2020-08-14 17:51:07 +0800

    settings

--- milky-common/src/main/java/cn/sliew/milky/common/settings/Setting.java
+++ milky-common/src/main/java/cn/sliew/milky/common/settings/Setting.java
@@ -1,155 +1,155 @@
-package cn.sliew.milky.common.settings;
-
-import com.sun.istack.internal.Nullable;
-
-import java.util.*;
-import java.util.function.Function;
-
-public class Setting<T> {
-
-    private static final EnumSet<Property> EMPTY_PROPERTIES = EnumSet.noneOf(Property.class);
-
-    private final Key key;
-    protected final Function<Settings, String> defaultValue;
-    @Nullable
-    private final Setting<T> fallbackSetting;
-    private final Function<String, T> parser;
-    private final Validator<T> validator;
-    private final EnumSet<Property> properties;
-
-    public Setting(Key key,
-                   Function<Settings, String> defaultValue,
-                   @Nullable Setting<T> fallbackSetting,
-                   Function<String, T> parser,
-                   Validator<T> validator,
-                   Property... properties) {
-        this.key = key;
-        this.defaultValue = defaultValue;
-        this.fallbackSetting = fallbackSetting;
-        this.parser = parser;
-        this.validator = validator;
-        if (properties == null) {
-            throw new IllegalArgumentException("properties cannot be null for setting [" + key + "]");
-        }
-        if (properties.length == 0) {
-            this.properties = EMPTY_PROPERTIES;
-        } else {
-            final EnumSet<Property> propertiesAsSet = EnumSet.copyOf(Arrays.asList(properties));
-            if (propertiesAsSet.contains(Property.Dynamic) && propertiesAsSet.contains(Property.Final)) {
-                throw new IllegalArgumentException("final setting [" + key + "] cannot be dynamic");
-            }
-            this.properties = propertiesAsSet;
-        }
-    }
-
-    /**
-     * Returns the settings key or a prefix if this setting is a group setting.
-     * <b>Note: this method should not be used to retrieve a value from a {@link Settings} object.
-     * Use {@link #get(Settings)} instead</b>
-     *
-     * @see #isGroupSetting()
-     */
-    public final String getKey() {
-        return key.toString();
-    }
-
-    /**
-     * Returns the original representation of a setting key.
-     */
-    public final Key getRawKey() {
-        return key;
-    }
-
-    /**
-     * Returns the settings value. If the setting is not present in the given settings object the default value is returned
-     * instead.
-     */
-    public T get(Settings settings) {
-        return get(settings, true);
-    }
-
-    private T get(Settings settings, boolean validate) {
-        String value = getRaw(settings);
-        try {
-            T parsed = parser.apply(value);
-            if (validate) {
-                final Iterator<Setting<T>> it = validator.settings();
-                final Map<Setting<T>, T> map;
-                if (it.hasNext()) {
-                    map = new HashMap<>();
-                    while (it.hasNext()) {
-                        final Setting<T> setting = it.next();
-                        map.put(setting, setting.get(settings, false)); // we have to disable validation or we will stack overflow
-                    }
-                } else {
-                    map = Collections.emptyMap();
-                }
-                validator.validate(parsed);
-                validator.validate(parsed, map);
-            }
-            return parsed;
-        } catch (ElasticsearchParseException ex) {
-            throw new IllegalArgumentException(ex.getMessage(), ex);
-        } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("Failed to parse value [" + value + "] for setting [" + getKey() + "]", ex);
-        } catch (IllegalArgumentException ex) {
-            throw ex;
-        } catch (Exception t) {
-            throw new IllegalArgumentException("Failed to parse value [" + value + "] for setting [" + getKey() + "]", t);
-        }
-    }
-
-    /**
-     * Returns the setting properties
-     *
-     * @see Property
-     */
-    public EnumSet<Property> getProperties() {
-        return properties;
-    }
-
-    /**
-     * Returns <code>true</code> if this setting is dynamically updateable, otherwise <code>false</code>
-     */
-    public final boolean isDynamic() {
-        return properties.contains(Property.Dynamic);
-    }
-
-    /**
-     * Returns <code>true</code> if this setting is final, otherwise <code>false</code>
-     */
-    public final boolean isFinal() {
-        return properties.contains(Property.Final);
-    }
-
-    /**
-     * Returns the default value for this setting.
-     *
-     * @param settings a settings object for settings that has a default value depending on another setting if available
-     */
-    public T getDefault(Settings settings) {
-        return parser.apply(defaultValue.apply(settings));
-    }
-
-    /**
-     * Returns true if and only if this setting is present in the given settings instance. Note that fallback settings are excluded.
-     *
-     * @param settings the settings
-     * @return true if the setting is present in the given settings instance, otherwise false
-     */
-    public boolean exists(final Settings settings) {
-        return settings.keySet().contains(getKey());
-    }
-
-    /**
-     * Returns true if and only if this setting including fallback settings is present in the given settings instance.
-     *
-     * @param settings the settings
-     * @return true if the setting including fallback settings is present in the given settings instance, otherwise false
-     */
-    public boolean existsOrFallbackExists(final Settings settings) {
-        return settings.keySet().contains(getKey()) || (fallbackSetting != null && fallbackSetting.existsOrFallbackExists(settings));
-    }
-
-
-}
+//package cn.sliew.milky.common.settings;
+//
+//import com.sun.istack.internal.Nullable;
+//
+//import java.util.*;
+//import java.util.function.Function;
+//
+//public class Setting<T> {
+//
+//    private static final EnumSet<Property> EMPTY_PROPERTIES = EnumSet.noneOf(Property.class);
+//
+//    private final Key key;
+//    protected final Function<Settings, String> defaultValue;
+//    @Nullable
+//    private final Setting<T> fallbackSetting;
+//    private final Function<String, T> parser;
+//    private final Validator<T> validator;
+//    private final EnumSet<Property> properties;
+//
+//    public Setting(Key key,
+//                   Function<Settings, String> defaultValue,
+//                   @Nullable Setting<T> fallbackSetting,
+//                   Function<String, T> parser,
+//                   Validator<T> validator,
+//                   Property... properties) {
+//        this.key = key;
+//        this.defaultValue = defaultValue;
+//        this.fallbackSetting = fallbackSetting;
+//        this.parser = parser;
+//        this.validator = validator;
+//        if (properties == null) {
+//            throw new IllegalArgumentException("properties cannot be null for setting [" + key + "]");
+//        }
+//        if (properties.length == 0) {
+//            this.properties = EMPTY_PROPERTIES;
+//        } else {
+//            final EnumSet<Property> propertiesAsSet = EnumSet.copyOf(Arrays.asList(properties));
+//            if (propertiesAsSet.contains(Property.Dynamic) && propertiesAsSet.contains(Property.Final)) {
+//                throw new IllegalArgumentException("final setting [" + key + "] cannot be dynamic");
+//            }
+//            this.properties = propertiesAsSet;
+//        }
+//    }
+//
+//    /**
+//     * Returns the settings key or a prefix if this setting is a group setting.
+//     * <b>Note: this method should not be used to retrieve a value from a {@link Settings} object.
+//     * Use {@link #get(Settings)} instead</b>
+//     *
+//     * @see #isGroupSetting()
+//     */
+//    public final String getKey() {
+//        return key.toString();
+//    }
+//
+//    /**
+//     * Returns the original representation of a setting key.
+//     */
+//    public final Key getRawKey() {
+//        return key;
+//    }
+//
+//    /**
+//     * Returns the settings value. If the setting is not present in the given settings object the default value is returned
+//     * instead.
+//     */
+//    public T get(Settings settings) {
+//        return get(settings, true);
+//    }
+//
+//    private T get(Settings settings, boolean validate) {
+//        String value = getRaw(settings);
+//        try {
+//            T parsed = parser.apply(value);
+//            if (validate) {
+//                final Iterator<Setting<T>> it = validator.settings();
+//                final Map<Setting<T>, T> map;
+//                if (it.hasNext()) {
+//                    map = new HashMap<>();
+//                    while (it.hasNext()) {
+//                        final Setting<T> setting = it.next();
+//                        map.put(setting, setting.get(settings, false)); // we have to disable validation or we will stack overflow
+//                    }
+//                } else {
+//                    map = Collections.emptyMap();
+//                }
+//                validator.validate(parsed);
+//                validator.validate(parsed, map);
+//            }
+//            return parsed;
+//        } catch (ElasticsearchParseException ex) {
+//            throw new IllegalArgumentException(ex.getMessage(), ex);
+//        } catch (NumberFormatException ex) {
+//            throw new IllegalArgumentException("Failed to parse value [" + value + "] for setting [" + getKey() + "]", ex);
+//        } catch (IllegalArgumentException ex) {
+//            throw ex;
+//        } catch (Exception t) {
+//            throw new IllegalArgumentException("Failed to parse value [" + value + "] for setting [" + getKey() + "]", t);
+//        }
+//    }
+//
+//    /**
+//     * Returns the setting properties
+//     *
+//     * @see Property
+//     */
+//    public EnumSet<Property> getProperties() {
+//        return properties;
+//    }
+//
+//    /**
+//     * Returns <code>true</code> if this setting is dynamically updateable, otherwise <code>false</code>
+//     */
+//    public final boolean isDynamic() {
+//        return properties.contains(Property.Dynamic);
+//    }
+//
+//    /**
+//     * Returns <code>true</code> if this setting is final, otherwise <code>false</code>
+//     */
+//    public final boolean isFinal() {
+//        return properties.contains(Property.Final);
+//    }
+//
+//    /**
+//     * Returns the default value for this setting.
+//     *
+//     * @param settings a settings object for settings that has a default value depending on another setting if available
+//     */
+//    public T getDefault(Settings settings) {
+//        return parser.apply(defaultValue.apply(settings));
+//    }
+//
+//    /**
+//     * Returns true if and only if this setting is present in the given settings instance. Note that fallback settings are excluded.
+//     *
+//     * @param settings the settings
+//     * @return true if the setting is present in the given settings instance, otherwise false
+//     */
+//    public boolean exists(final Settings settings) {
+//        return settings.keySet().contains(getKey());
+//    }
+//
+//    /**
+//     * Returns true if and only if this setting including fallback settings is present in the given settings instance.
+//     *
+//     * @param settings the settings
+//     * @return true if the setting including fallback settings is present in the given settings instance, otherwise false
+//     */
+//    public boolean existsOrFallbackExists(final Settings settings) {
+//        return settings.keySet().contains(getKey()) || (fallbackSetting != null && fallbackSetting.existsOrFallbackExists(settings));
+//    }
+//
+//
+//}
--- milky-common/src/main/java/cn/sliew/milky/common/settings/Validator.java
+++ milky-common/src/main/java/cn/sliew/milky/common/settings/Validator.java
@@ -1,38 +1,38 @@
-package cn.sliew.milky.common.settings;
-
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Map;
-
-@FunctionalInterface
-public interface Validator<T> {
-
-    /**
-     * Validate this setting's value in isolation.
-     *
-     * @param value the value of this setting
-     */
-    void validate(T value);
-
-    /**
-     * Validate this setting against its dependencies, specified by {@link #settings()}. The default implementation does nothing,
-     * accepting any value as valid as long as it passes the validation in {@link #validate(Object)}.
-     *
-     * @param value    the value of this setting
-     * @param settings a map from the settings specified by {@link #settings()}} to their values
-     */
-    default void validate(T value, Map<Setting<T>, T> settings) {
-    }
-
-
-    /**
-     * The settings on which the validity of this setting depends. The values of the specified settings are passed to
-     * {@link #validate(Object, Map)}. By default this returns an empty iterator, indicating that this setting does not depend on any
-     * other settings.
-     *
-     * @return the settings on which the validity of this setting depends.
-     */
-    default Iterator<Setting<T>> settings() {
-        return Collections.emptyIterator();
-    }
-}
+//package cn.sliew.milky.common.settings;
+//
+//import java.util.Collections;
+//import java.util.Iterator;
+//import java.util.Map;
+//
+//@FunctionalInterface
+//public interface Validator<T> {
+//
+//    /**
+//     * Validate this setting's value in isolation.
+//     *
+//     * @param value the value of this setting
+//     */
+//    void validate(T value);
+//
+//    /**
+//     * Validate this setting against its dependencies, specified by {@link #settings()}. The default implementation does nothing,
+//     * accepting any value as valid as long as it passes the validation in {@link #validate(Object)}.
+//     *
+//     * @param value    the value of this setting
+//     * @param settings a map from the settings specified by {@link #settings()}} to their values
+//     */
+//    default void validate(T value, Map<Setting<T>, T> settings) {
+//    }
+//
+//
+//    /**
+//     * The settings on which the validity of this setting depends. The values of the specified settings are passed to
+//     * {@link #validate(Object, Map)}. By default this returns an empty iterator, indicating that this setting does not depend on any
+//     * other settings.
+//     *
+//     * @return the settings on which the validity of this setting depends.
+//     */
+//    default Iterator<Setting<T>> settings() {
+//        return Collections.emptyIterator();
+//    }
+//}
