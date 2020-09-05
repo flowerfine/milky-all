11ff0c8b54887e677dfc3ef7f655391bb8e0b2d2
wangqi <1942460489@qq.com>
2020-09-06 00:17:07 +0800

    配置日志

---
+++ milky-test/src/test/java/cn/sliew/milky/test/MilkyTestCase.java
@@ -0,0 +1,14 @@
+package cn.sliew.milky.test;
+
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MilkyTestCase {
+
+    private static final Logger log = LoggerFactory.getLogger(MilkyTestCase.class);
+
+    public static void main(String[] args) {
+        log.info("hhhhhh");
+    }
+}
