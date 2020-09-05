4db8b09363b335d494e619d7bb007339138b1907
wangqi <1942460489@qq.com>
2020-09-06 00:58:44 +0800

    测试jupiter的api

--- milky-test/src/test/java/cn/sliew/milky/test/MilkyTestCase.java
+++ milky-test/src/test/java/cn/sliew/milky/test/MilkyTestCase.java
@@ -1,14 +1,22 @@
 package cn.sliew.milky.test;
 
-import ch.qos.logback.classic.LoggerContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.*;
 
 public class MilkyTestCase {
 
-    private static final Logger log = LoggerFactory.getLogger(MilkyTestCase.class);
-
-    public static void main(String[] args) {
-        log.info("hhhhhh");
+    @Test
+    public void testReporterInfoInject(TestReporter reporter) {
+        reporter.publishEntry(getClass().getSimpleName());
     }
+
+    @Test
+    public void testTestInfoInject(TestInfo test) {
+        System.out.println(test);
+    }
+
+    @RepeatedTest(10)
+    public void testRepetitionInfoInject(RepetitionInfo repetition) {
+        System.out.println(repetition);
+    }
+
 }
