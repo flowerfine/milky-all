3c6d4d16425e33012761bc569a80c868c2c5240e
wangqi <wangqi@xinc818.group>
2021-01-10 11:22:58 +0800

    feature: 添加 dubbo channel buffer

---
+++ milky-remote/src/main/java/cn/sliew/milky/remote/buffer/HeapChannelBufferFactory.java
@@ -0,0 +1,40 @@
+package cn.sliew.milky.remote.buffer;
+
+import java.nio.ByteBuffer;
+
+public class HeapChannelBufferFactory implements ChannelBufferFactory {
+
+    private static final HeapChannelBufferFactory INSTANCE = new HeapChannelBufferFactory();
+
+    public HeapChannelBufferFactory() {
+        super();
+    }
+
+    public static ChannelBufferFactory getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ChannelBuffer getBuffer(int capacity) {
+        return ChannelBuffers.buffer(capacity);
+    }
+
+    @Override
+    public ChannelBuffer getBuffer(byte[] array, int offset, int length) {
+        return ChannelBuffers.wrappedBuffer(array, offset, length);
+    }
+
+    @Override
+    public ChannelBuffer getBuffer(ByteBuffer nioBuffer) {
+        if (nioBuffer.hasArray()) {
+            return ChannelBuffers.wrappedBuffer(nioBuffer);
+        }
+
+        ChannelBuffer buf = getBuffer(nioBuffer.remaining());
+        int pos = nioBuffer.position();
+        buf.writeBytes(nioBuffer);
+        nioBuffer.position(pos);
+        return buf;
+    }
+
+}
