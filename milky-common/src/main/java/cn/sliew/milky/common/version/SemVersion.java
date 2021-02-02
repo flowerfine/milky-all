193c0710d207e7bfde291dfc4692b48799552e06
wangqi <1942460489@qq.com>
2021-02-02 08:56:05 +0800

    feature: version

---
+++ milky-common/src/main/java/cn/sliew/milky/common/version/SemVersion.java
@@ -0,0 +1,69 @@
+package cn.sliew.milky.common.version;
+
+/**
+ * semantic versioning implementions.
+ *
+ * @see <a href="http://semver.org" />
+ */
+public class SemVersion {
+
+    private static final String REGEX = "(0|[1-9]d*).(0|[1-9]d*).(0|[1-9]d*)";
+
+    private final int major;
+    private final int minor;
+    private final int patch;
+
+    public SemVersion(int major, int minor, int patch) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+
+    public static SemVersion semVersion() {
+        return new SemVersion(0, 0, 1);
+    }
+
+    public static SemVersion semVersion(int major, int minor, int patch) {
+        if ((major == 0 && minor == 0 && patch == 0) || (major < 0 || minor < 0 || patch < 0)) {
+            throw new IllegalArgumentException(String.format("bad semantic version %d.%d.%d must not be negative", major, minor, patch));
+        }
+        return new SemVersion(major, minor, patch);
+    }
+
+    public static SemVersion semVersion(String version) {
+        try {
+            String[] parts = new String[0];
+            if (version != null && !version.isEmpty()) {
+                version.split(".");
+            }
+
+            int major = 0;
+            if (parts.length >= 1) {
+                major = Integer.parseInt(parts[0]);
+            }
+            int minor = 0;
+            if (parts.length >= 2) {
+                minor = Integer.parseInt(parts[1]);
+            }
+            int patch = 0;
+            if (parts.length >= 3) {
+                patch = Integer.parseInt(parts[2]);
+            }
+            return semVersion(major, minor, patch);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("bad semantic version %s", version));
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%d.%d.%d", major, minor, patch);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SemVersion.semVersion(1, 1, -1));
+    }
+
+
+}
