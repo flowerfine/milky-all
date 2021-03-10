4ccfb9a4c4dfaa1a041c039a6d310ff95fd774c8
wangqi <wangqi@xinc818.group>
2021-03-10 18:31:03 +0800

    SerializableObjectSizeEstimator component.

--- milky-objectsize/src/main/java/cn/sliew/milky/objectsize/ObjectSizeOfStrategy.java
+++ milky-objectsize/src/main/java/cn/sliew/milky/objectsize/ObjectSizeOfStrategy.java
@@ -1,7 +1,5 @@
 package cn.sliew.milky.objectsize;
 
-import java.io.Serializable;
-
 public interface ObjectSizeOfStrategy {
 
     /**
@@ -10,7 +8,5 @@ public interface ObjectSizeOfStrategy {
      * @param obj The serializable object to compute size of
      * @return The size of the object in bytes. failed with -1.
      */
-    long sizeOf(Serializable obj);
-
     long sizeOf(Object obj);
 }
