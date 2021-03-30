83f1408f2cf3158b9b5410671431cbe257720a1f
wangqi <wangqi@xinc818.group>
2021-03-30 13:57:43 +0800

    milky-log module.

---
+++ milky-log/src/main/java/cn/sliew/milky/log/Log4J2LoggerFactory.java
@@ -0,0 +1,14 @@
+package cn.sliew.milky.log;
+
+
+import org.apache.logging.log4j.LogManager;
+
+public final class Log4J2LoggerFactory extends LoggerFactory {
+
+    public static final LoggerFactory INSTANCE = new Log4J2LoggerFactory();
+
+    @Override
+    public Logger newInstance(String name) {
+        return new Log4J2Logger(LogManager.getLogger(name));
+    }
+}
---
+++ milky-log/src/main/java/cn/sliew/milky/log/LogLevel.java
@@ -0,0 +1,27 @@
+package cn.sliew.milky.log;
+
+/**
+ * The log level that {@link Logger} can log at.
+ */
+public enum LogLevel {
+    /**
+     * 'TRACE' log level.
+     */
+    TRACE,
+    /**
+     * 'DEBUG' log level.
+     */
+    DEBUG,
+    /**
+     * 'INFO' log level.
+     */
+    INFO,
+    /**
+     * 'WARN' log level.
+     */
+    WARN,
+    /**
+     * 'ERROR' log level.
+     */
+    ERROR
+}
