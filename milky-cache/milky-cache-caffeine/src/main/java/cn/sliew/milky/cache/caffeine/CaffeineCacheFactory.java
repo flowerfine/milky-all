6f0cfdf1375b48dca5ac4f31e6dd9fe5e8b36441
wangqi <wangqi@xinc818.group>
2021-06-28 18:23:52 +0800

    feature/version: change master version to 1.0.2-SNAPSHOT.

--- milky-cache/milky-cache-caffeine/src/main/java/cn/sliew/milky/cache/caffeine/CaffeineCacheFactory.java
+++ milky-cache/milky-cache-caffeine/src/main/java/cn/sliew/milky/cache/caffeine/CaffeineCacheFactory.java
@@ -3,13 +3,14 @@ package cn.sliew.milky.cache.caffeine;
 import cn.sliew.milky.cache.AbstractCacheFactory;
 import cn.sliew.milky.cache.CacheOptions;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static cn.sliew.milky.common.check.Ensures.checkArgument;
 
 public class CaffeineCacheFactory extends AbstractCacheFactory<CaffeineCache> {
 
     @Override
     protected CaffeineCache newCache(CacheOptions options) {
         checkArgument(options instanceof CaffeineCacheOptions);
+
         CaffeineCacheOptions caffeineOptions = (CaffeineCacheOptions) options;
         return new CaffeineCache(caffeineOptions);
     }
