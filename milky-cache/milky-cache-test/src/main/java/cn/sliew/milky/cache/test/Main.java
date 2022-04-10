43a3333bec57aee52aeefd3c73a3b480092a8417
wangqi <1942460489@qq.com>
2022-04-10 08:30:24 +0800

    release

--- milky-cache/milky-cache-test/src/main/java/cn/sliew/milky/cache/test/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.sliew.milky.cache.test;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
--- milky-event/milky-event-test/src/main/java/cn/sliew/milky/event/test/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.sliew.milky.event.test;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
